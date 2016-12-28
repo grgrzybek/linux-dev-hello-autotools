f47c885b6a8118fba5061d32a4fe536f374c286d
Grzegorz Grzybek <gr.grzybek@gmail.com>
2016-12-28 16:03:01 +0100

    Initial code with README, .gitignore and one source file

---
+++ src/hello.c
@@ -0,0 +1,15 @@
+/*
+ * main.c
+ *
+ *  Created on: Jun 10, 2015
+ *      Author: ggrzybek
+ */
+
+#include <stdio.h>
+
+int
+main(int argc, char** argv) {
+	printf("Hello %s!\n", "World");
+
+	return 0;
+}
