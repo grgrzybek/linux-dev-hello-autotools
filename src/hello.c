3c35383fd088ff52049e8a35a3d50c7ec9a0e451
Grzegorz Grzybek <gr.grzybek@gmail.com>
2016-12-29 12:27:05 +0100

    Add #include <config.h> in source, to link with autotools artifacts - not working yet

--- src/hello.c
+++ src/hello.c
@@ -6,10 +6,11 @@
  */
 
 #include <stdio.h>
+#include <config.h>
 
 int
 main(int argc, char** argv) {
-	printf("Hello %s!\n", "World");
+	printf("Hello %s (%s)!\n", "World", PACKAGE_STRING);
 
 	return 0;
 }
